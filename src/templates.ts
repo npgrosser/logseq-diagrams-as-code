9302f1a89bb2f531df1f1de62beab6491aca6039
Nicolai Großer <nicolai.grosser@googlemail.com>
2022-02-23 00:24:54 +0100

    added nwdiag template

--- src/templates.ts
+++ src/templates.ts
@@ -135,6 +135,23 @@ const templates: Template[] = [
       { name: "bus",  wave: "x.==.=x", data: ["head", "body", "tail", "data"] },
       { name: "wire", wave: "0.1..0." }
     ]}`)),
+    new InMemoryTemplate("nwdiag", "NwDiag Diagram", dedent(`
+    nwdiag {
+      network dmz {
+          address = "210.x.x.x/24"
+    
+          web01 [address = "210.x.x.1"];
+          web02 [address = "210.x.x.2"];
+      }
+      network internal {
+          address = "172.x.x.x/24";
+    
+          web01 [address = "172.x.x.1"];
+          web02 [address = "172.x.x.2"];
+          db01;
+          db02;
+      }
+    }`)),
 ]
 
 
