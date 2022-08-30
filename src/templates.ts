91570a6f69ec176b20ce4d6fc29ebd42be4c6b2d
Dom <dominik.stamm@me.com>
2022-08-30 14:26:48 +0200

    added umlet support and template for bytefield diagram

--- src/templates.ts
+++ src/templates.ts
@@ -1,4 +1,4 @@
-import {dedent} from "./lib/string-utils";
+import { dedent } from './lib/string-utils';
 
 export interface Template {
     readonly rendererType: string
@@ -904,6 +904,71 @@ const templates: Template[] = [
       }
     }`)),
     new InMemoryTemplate("bytefield", "Bytefield Illustration", dedent(`
+    (defattrs :bg-green {:fill "#a0ffa0"})
+    (defattrs :bg-yellow {:fill "#ffffa0"})
+    (defattrs :bg-pink {:fill "#ffb0a0"})
+    (defattrs :bg-cyan {:fill "#a0fafa"})
+    (defattrs :bg-purple {:fill "#e4b5f7"})
+
+    (defn draw-group-label-header
+      [span label]
+      (draw-box (text label [:math {:font-size 12}]) {:span span :borders #{} :height 14}))
+
+    (defn draw-remotedb-header
+      [kind args]
+      (draw-column-headers)
+      (draw-group-label-header 5 "start")
+      (draw-group-label-header 5 "TxID")
+      (draw-group-label-header 3 "type")
+      (draw-group-label-header 2 "args")
+      (draw-group-label-header 1 "tags")
+      (next-row 18)
+
+      (draw-box 0x11 :bg-green)
+      (draw-box 0x872349ae [{:span 4} :bg-green])
+      (draw-box 0x11 :bg-yellow)
+      (draw-box (text "TxID" :math) [{:span 4} :bg-yellow])
+      (draw-box 0x10 :bg-pink)
+      (draw-box (hex-text kind 4 :bold) [{:span 2} :bg-pink])
+      (draw-box 0x0f :bg-cyan)
+      (draw-box (hex-text args 2 :bold) :bg-cyan)
+      (draw-box 0x14 :bg-purple)
+
+      (draw-box (text "0000000c" :hex [[:plain {:font-weight "light" :font-size 16}] " (12)"]) [{:span 4} :bg-purple])
+      (draw-box (hex-text 6 2 :bold) [:box-first :bg-purple])
+      (doseq [val [6 6 3 6 6 6 6 3]]
+        (draw-box (hex-text val 2 :bold) [:box-related :bg-purple]))
+      (doseq [val [0 0]]
+        (draw-box val [:box-related :bg-purple]))
+      (draw-box 0 [:box-last :bg-purple]))
+
+    (draw-remotedb-header 0x4702 9)
+
+    (draw-box 0x11)
+    (draw-box 0x2104 {:span 4})
+    (draw-box 0x11)
+    (draw-box 0 {:span 4})
+    (draw-box 0x11)
+    (draw-box (text "length" [:math] [:sub 1]) {:span 4})
+    (draw-box 0x14)
+
+    (draw-box (text "length" [:math] [:sub 1]) {:span 4})
+    (draw-gap "Cue and loop point bytes")
+
+    (draw-box nil :box-below)
+    (draw-box 0x11)
+    (draw-box 0x36 {:span 4})
+    (draw-box 0x11)
+    (draw-box (text "num" [:math] [:sub "hot"]) {:span 4})
+    (draw-box 0x11)
+    (draw-box (text "num" [:math] [:sub "cue"]) {:span 4})
+
+    (draw-box 0x11)
+    (draw-box (text "length" [:math] [:sub 2]) {:span 4})
+    (draw-box 0x14)
+    (draw-box (text "length" [:math] [:sub 2]) {:span 4})
+    (draw-gap "Unknown bytes" {:min-label-columns 6})
+    (draw-bottom)
 `)),
     new InMemoryTemplate("packetdiag", "PacketDiag Diagram", dedent(`
     packetdiag {
@@ -1624,7 +1689,10 @@ const templates: Template[] = [
     "viewBackgroundColor": "#ffffff",
     "gridSize": 20
   }
-}`))
+}`)),
+    new InMemoryTemplate("umlet", "UMLet Diagram", dedent(`
+    <diagram program="umletino" version="15.0.0"><zoom_level>10</zoom_level><element><id>UMLClass</id><coordinates><x>90</x><y>80</y><w>100</w><h>30</h></coordinates><panel_attributes>Class A</panel_attributes><additional_attributes></additional_attributes></element><element><id>UMLClass</id><coordinates><x>90</x><y>160</y><w>100</w><h>30</h></coordinates><panel_attributes>Class B</panel_attributes><additional_attributes></additional_attributes></element><element><id>Relation</id><coordinates><x>130</x><y>100</y><w>30</w><h>80</h></coordinates><panel_attributes>lt=&lt;&lt;-</panel_attributes><additional_attributes>10;10;10;60</additional_attributes></element></diagram>
+`))
 ];
 
 
